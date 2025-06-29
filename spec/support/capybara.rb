886b5081b1b56e46fa9fbac4634eb43764b9315e
cristianv77 <vargas.t.cristian@gmail.com>
2025-06-29 20:41:10 -0300

    Add feature test (#15)

---
+++ spec/support/capybara.rb
@@ -0,0 +1,22 @@
+require 'capybara/rspec'
+
+Capybara.register_driver :selenium_chrome_headless do |app|
+  options = Selenium::WebDriver::Chrome::Options.new
+  options.add_argument('--headless')
+  options.add_argument('--no-sandbox')
+  options.add_argument('--disable-dev-shm-usage')
+  options.add_argument('--disable-gpu')
+  options.add_argument('--window-size=1400,1400')
+
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+end
+
+if ENV['CI']
+  Capybara.default_driver = :selenium_chrome_headless
+  Capybara.javascript_driver = :selenium_chrome_headless
+end
+
+Capybara.configure do |config|
+  config.default_max_wait_time = 5
+  config.server = :puma, { Silent: true }
+end
